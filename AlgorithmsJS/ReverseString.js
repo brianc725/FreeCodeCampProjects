206de8b43311815e95dbd1e6769dca9d3a59c4bf
brianc725 <brianc725@gmail.com>
2017-01-02 01:12:38 -0800

    Delete ReverseString.js

--- AlgorithmsJS/ReverseString.js
+++
@@ -1,9 +0,0 @@
-
-function reverseString(str) {
-  var strSplit = str.split('');
-  var strRev = strSplit.reverse();
-  var strJoin = strRev.join('');
-  return strJoin;
-}
-
-reverseString("hello");
